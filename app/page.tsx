abc0e6df2e1d7910985650be39c77a1d7621a516
why-akash <mr.akash5414@gmail.com>
2026-08-21 20:45:26 +0530

    remove

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ import WhyUs from '@/components/home/WhyUs'
 import Services from '@/components/home/Services'
 import FeaturedProjects from '@/components/home/FeaturedProjects'
 import CustomSolutionCTA from '@/components/home/CustomSolutionCTA'
-import AnniversaryOffer from '@/components/home/AnniversaryOffer'
 
 export default function Home() {
   return (
     <SiteCanvas>
       <Hero />
       <div className="relative z-[1]">
-        <AnniversaryOffer />
         <WhyUs />
         <Services />
         <FeaturedProjects />
